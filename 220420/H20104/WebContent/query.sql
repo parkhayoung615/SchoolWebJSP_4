e49b1afded9347d8c54f566b415450b61f4387d5
parkhayoung615 <parkhayoung615@gmail.com>
2022-04-20 10:53:33 +0900

    Update Project
    2022.04.20 16 commit

--- 220420/H20104/WebContent/query.sql
+++ 220420/H20104/WebContent/query.sql
@@ -3,4 +3,10 @@ id varchar2(5),
 pwd varchar(5)
 );
 
+INSERT INTo HelloProj values ('aa', '11');
+INSERT INTo HelloProj values ('bb', '22');
+INSERT INTo HelloProj values ('cc', '33');
+
 SELECT * FROM HelloProj;
+
+DROP table HelloProj;
